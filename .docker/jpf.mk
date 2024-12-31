bd313df61761d934914df87a9a045ce79c5b311c
Sylvain VANEL <sylvain.vanel@kleegroup.com>
2024-12-31 11:44:54 +0100

    Make command instead of script for fixperm

--- .docker/jpf.mk
+++ .docker/jpf.mk
@@ -1,4 +1,11 @@
+include .env
+
 ## quality	:	Executes grumphp to check quality.
 .PHONY: quality
 quality:
 	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") vendor/bin/grumphp run
+
+## fixperm	:	Fix perm on Windows (fpm root).
+.PHONY: fixperm
+fixperm:
+	sudo chown -R $(WODBY_USER_ID):$(WODBY_GROUP_ID) .
--- scripts/fixperm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Abort if anything fails
-set -e
-
-source .env
-
-sudo chown -R "$WODBY_USER_ID:$WODBY_GROUP_ID" .
